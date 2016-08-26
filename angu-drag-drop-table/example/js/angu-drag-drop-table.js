458e5c588453869afe420886666ee474be07257f
KarateJB <ppippenkimo@gamil.com>
2016-08-26 14:29:28 +0800

    Can catch the drag and drop value.

--- angu-drag-drop-table/example/js/angu-drag-drop-table.js
+++ angu-drag-drop-table/example/js/angu-drag-drop-table.js
@@ -1,31 +1,139 @@
 angular.module('anguDragDropTable', [])
-    .directive('anguDragDropTable', function () {
+    .directive('anguDrag', function ($timeout, $document) {
+
+        return {
+            // restrict: "A",
+            scope: {
+                columns: "=ngModel"
+            },
+            link: function ($scope, $element, $attr) {
+
+                $element.bind('dragstart', function (event) {
+                    var dragColId = event.target.id;
+                    var isStop = false;
+                    angular.forEach($scope.columns.data, function (col) {
+                        if (!isStop) {
+                            if (dragColId === col.id) {
+                                $scope.columns.dragColumn = col;
+                                $scope.$apply();
+                                isStop = true;
+                            }
+                        }
+                    })
+                })
+
+            },
+            controller: function ($scope, $element) {
+
+            }
+
+        }
+    })
+    .directive('anguDrop', function ($timeout, $document) {
+
+        return {
+            // restrict: "A",
+            scope: {
+                columns: "=ngModel"
+            },
+            link: function ($scope, $element, $attr) {
+
+                // var queryRslt = $document[0].getElementById($scope.elemId);
+                // var elem = angular.element(queryRslt);
+
+                $element.bind('drop', function (event) {
+
+
+                    var dropColId = event.target.id;
+
+                    var isStop = false;
+                    angular.forEach($scope.columns.data, function (col) {
+                        if (!isStop) {
+                            if (dropColId === col.id) {
+                                $scope.columns.dropColumn = col;
+                                $scope.$apply();
+                                isStop = true;
+                            }
+                        }
+                    })
+
+
+
+                    // event.stopPropagation();
+                    event.preventDefault();
+                });
+
+                $element.bind('dragover', function (event) {
+                    event.preventDefault();
+                });
+
+            },
+            controller: function ($scope, $element) {
+
+            }
+
+        }
+    })
+    .directive('anguDragDropTable', function ($document) {
 
         var template = `
+        <input type ="text" ng-model="columns.dragColumn" value="{{columns.dragColumn.id}}" />
+        <input type ="text" ng-model="columns.dropColumn" value="{{columns.dropColumn.id}}" />
         <table ng-class="tableClass">
         <thead>
         <tr>
-        <td ng-repeat="col in columns">{{col.title}}<td>
+        <td ng-repeat="col in src.columns.data  | orderBy:'order'">
+          <div class="form-group">
+            <div class="col-md-6" angu-drag ng-model="src.columns" id="{{col.id}}" draggable="true">
+                {{col.title}}
+            </div>
+            <div angu-drop ng-model="src.columns" id="{{col.id}}"
+                class="col-md-6" style="background-color:lightblue;min-width:50%;min-height:50%">
+            &nbsp;
+            </div>
+          </div>
+          <td>
         </tr>
         </thead>
         <tbody>
-        <tr ng-repeat="row in localData">
+        <tr ng-repeat="row in src.rows">
            <td ng-repeat="data in row.Data">{{data.value}}</td>
         </tr>
         </tbody>
         </table>
+        <input type="button" value="Show" ng-click="Show()" />
         `;
 
         return {
             // restrict: "A",
             scope: {
-                localData: '=ngModel',  //Row data
-                columns: '=',          //Columns
+                src: '=ngModel',  //Row data
                 tableClass: '@'         //The Css class of table
             },
             template: template,
             link: function ($scope, $element, $attr) {
-                console.log($scope.tableClass);
+
+                $scope.dragColumn = null;
+                $scope.dropColumn = null;
+                $scope.Show = function () {
+                    console.log($scope.src.columns.dragColumn);
+                    console.log($scope.src.columns.dropColumn);
+
+                }
+
+                // $scope.initColumns = function () {
+
+                //     angular.forEach($scope.columns, function (col) {
+
+                //         var queryRslt = $document[0].getElementById(id)
+                //         var elem = angular.element(queryRslt);
+
+                //     })
+
+
+                // }
+
+                // $scope.initColumns();
             },
             controller: function ($scope, $element) {
 
--- angu-drag-drop-table/example/js/demo.js
+++ angu-drag-drop-table/example/js/demo.js
@@ -4,8 +4,11 @@ angular.module('app', ['anguDragDropTable'])
         $scope.tableClass = "table-bordered";
         $scope.src= {'columns':[], 'localData':[]}
 
-        $scope.src.columns=[{'title':'SN'},{'title':'Name'},{'title':'Age'}];
-        $scope.src.localData=[
+        $scope.src.columns={'dragColumn':{}, 'dropColumn':{},
+            'data':[{'id':'colSn','order':1,'title':'SN'},
+            {'id':'colName','order':2,'title':'Name'},
+            {'id':'colAge', 'order':3,'title':'Age'}]};
+        $scope.src.rows=[
             {'Data':[{'column':'SN','value':1},{'column':'Name','value':'JB'},{'column':'Age','value':35}]},
             {'Data':[{'column':'SN','value':2},{'column':'Name','value':'Lily'},{'column':'Age','value':18}]},
             {'Data':[{'column':'SN','value':3},{'column':'Name','value':'Leia'},{'column':'Age','value':3}]},
